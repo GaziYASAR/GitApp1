c41fb29e61584c50021a7ee6ca1d19bb124722b6
Gazi YASAR <gazibey@live.com>
2024-05-24 23:53:02 +0400

    v1

---
+++ src/main/java/Runner.java
@@ -0,0 +1,12 @@
+public class Runner {
+    public static void main(String[] args) {
+
+        //version 1
+        System.out.print("Hello git");
+        System.out.println("git add . staging areaya alir");
+
+        //version 2
+
+        System.out.println("Hello");
+    }
+}
